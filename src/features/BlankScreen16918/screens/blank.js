dc4ece9767db3312c17b2848f79b9faddb1de111
Crowdbotics <team@crowdbotics.com>
2020-06-30 16:46:48 +0000

    Update components

--- src/features/BlankScreen16918/screens/blank.js
+++ src/features/BlankScreen16918/screens/blank.js
@@ -271,7 +271,11 @@ export class _Blank extends React.Component {
             }}
           />
         </View>
-        <View
+        <ImageBackground
+          source={{
+            uri:
+              "https://d3nfddh2sbo9hk.cloudfront.net/media/project_component_resources/0-Slack-Image.jpg"
+          }}
           style={{
             width: "100%",
             height: "50%",
@@ -307,39 +311,7 @@ export class _Blank extends React.Component {
             borderRadius: 0,
             backgroundSize: "auto"
           }}
-        >
-          <Image
-            resizeMode="cover"
-            source={{
-              uri:
-                "https://d3nfddh2sbo9hk.cloudfront.net/media/project_component_resources/1-slack.png"
-            }}
-            style={{
-              width: "100%",
-              height: "100%",
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 0,
-              marginBottom: 0,
-              paddingLeft: 0,
-              paddingRight: 0,
-              paddingTop: 0,
-              paddingBottom: 0,
-              overflow: "visible",
-              textAlign: "left",
-              verticalAlign: "baseline",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0,
-              fontFamily: "System"
-            }}
-          />
-        </View>
+        />
       </View>
     </View>
   )
